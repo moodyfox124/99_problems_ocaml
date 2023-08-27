ae9015a73bbfc486f796c2db4d36ad8d0ec659fe
moodyfox124 <moodyfox124@gmail.com>
2023-08-27 21:25:38 +0300

    fixed input data for the problem_1

--- bin/problem_1.ml
+++ bin/problem_1.ml
@@ -1,5 +1,7 @@
-let input_data1 = ["a"; "b"; "c"; "d"];;
-let _input_data2 = [];;
+let input_data1: ('a list) = ["a"; "b"; "c"; "d"];;
+let input_data2 = [];;
+
+let input_data = [input_data1; input_data2];;
 
 let rec last l =
   match l with
@@ -7,6 +9,9 @@ let rec last l =
   | [x] -> Some x
   | _ :: rest -> last rest;;
 
-let () = match last input_data1 with
-|None -> print_endline "Empty list"
-|Some x -> print_endline x;;
+let print_result_opt r =
+  match r with
+  | None -> print_endline "No result"
+  | Some x -> print_endline x;;
+
+let () = List.iter print_result_opt (List.map last input_data);;
